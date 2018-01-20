4a25697522c45c55b0f2e17d37bf9ad2adba262f
qkrwogk110 <qkrwogk110@naver.com>
2018-01-21 02:27:34 +0900

    change

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function playStream(idVideoTag, stream){
 //openStream()
 //.then(stream => playStream('localStream', stream));
 
-const peer = new Peer({key: 'wkr3olzfv9rudi'});
+const peer = new Peer({key: 'peerjs', host: 'mypeer1801.herokuapp.com' , secure: true, port: 443 });
 
 peer.on('open', id => {
   $('#my-peer').append(id);
